6215590138809c263729b8628568aba43bd622dc
Thanh Thy <thtt.it1502@gmail.com>
2023-12-16 17:05:08 +0700

    update

--- config/server.js
+++ config/server.js
@@ -1,11 +1,20 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("", "https://nirni-store.onrender.com"),
+  url: env("", "https://nirnioffice.com/"),
   app: {
     keys: env.array("APP_KEYS"),
   },
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  server: {
+    ssl: {
+      enabled: true,
+      redirectHttpFromPort: 80,
+      httpPort: 1337,
+      cert: env("SSL_CERT", ""),
+      key: env("SSL_KEY", ""),
+    },
   },
 });
