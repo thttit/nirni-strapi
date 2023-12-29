2192eca85d3134d54a8439757e960f82e3bc9e6e
Thanh Thy <thtt.it1502@gmail.com>
2023-12-29 15:25:59 +0700

    update

--- config/database.js
+++ config/database.js
@@ -4,15 +4,22 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env(
-        "DATABASE_HOST",
-        "nirni-database.cbyiukdfzhok.ap-southeast-1.rds.amazonaws.com"
-      ),
+      host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "nirni-database"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "nirnishop000"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
     },
+    // connection: {
+    //   host: env(
+    //     "DATABASE_HOST",
+    //     "nirni-database.cbyiukdfzhok.ap-southeast-1.rds.amazonaws.com"
+    //   ),
+    //   port: env.int("DATABASE_PORT", 5432),
+    //   database: env("DATABASE_NAME", "nirni-database"),
+    //   user: env("DATABASE_USERNAME", "postgres"),
+    //   password: env("DATABASE_PASSWORD", "nirnishop000"),
+    // },
     useNullAsDefault: true,
   },
 });
