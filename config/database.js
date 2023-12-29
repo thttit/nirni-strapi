fc7c555ccb0edf023c94a497568d8b85313796ed
Thanh Thy <thtt.it1502@gmail.com>
2023-12-29 19:16:29 +0700

    update n

--- config/database.js
+++ config/database.js
@@ -6,10 +6,10 @@ module.exports = ({ env }) => ({
     connection: {
       host: env(
         "DATABASE_HOST",
-        "nirni-database.cbyiukdfzhok.ap-southeast-1.rds.amazonaws.com"
+        "nirni-db.cbyiukdfzhok.ap-southeast-1.rds.amazonaws.com"
       ),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", ""),
+      database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "nirnishop000"),
       ssl: {
