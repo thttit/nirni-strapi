a2bb8c105656c5c308bbdd6e4a51876da3e001b6
Thanh Thy <thtt.it1502@gmail.com>
2023-12-29 15:05:29 +0700

    update deploy cloudary

--- config/plugins.js
+++ config/plugins.js
@@ -1,25 +1,15 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "aws-s3",
+      provider: "cloudinary",
       providerOptions: {
-        s3Options: {
-          accessKeyId: env("AWS_ACCESS_KEY_ID"),
-          secretAccessKey: env("AWS_ACCESS_SECRET"),
-          region: env("AWS_REGION"),
-          params: {
-            Bucket: env("AWS_BUCKET_NAME"),
-          },
-        },
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
-      // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
       actionOptions: {
-        upload: {
-          ACL: null,
-        },
-        uploadStream: {
-          ACL: null,
-        },
+        upload: {},
+        delete: {},
       },
     },
   },
