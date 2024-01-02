315856ef0f05e4f28a94b67bac8b8de7b58f5c59
Thanh Thy <thtt.it1502@gmail.com>
2024-01-02 19:05:51 +0700

    update domain vn

--- config/server.js
+++ config/server.js
@@ -1,12 +1,12 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("", "https://nirnioffice.com/"),
+  // url: env("", "https://nirnioffice.com/"),
   app: {
     keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", true),
   },
   server: {
     ssl: {
