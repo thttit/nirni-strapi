35bfaef771c0a3fcfdef5d81e5bc05635430aeec
Thanh Thy <thtt.it1502@gmail.com>
2024-01-03 22:35:48 +0700

    update v2

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://nirnioffice.com",
+  // url: "https://nirnioffice.com",
   app: {
     keys: env.array("APP_KEYS"),
   },
